264f29983857f92a12e1d8628c014634cbaa41ac
Thiago Dallacqua <thiagodallacqua@outlook.com>
2019-08-15 08:34:18 +0200

    added the cors exception for localhost.

--- config/express.js
+++ config/express.js
@@ -12,6 +12,8 @@ module.exports = function() {
 
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3333/");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
     next();
   });
